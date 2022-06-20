22cd5f07f4538f4adca7ad02adf809fae1521d1d
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2022-06-20 23:28:02 +0600

    feat: first build

---
+++ app/src/main/java/kaz/dev/thoughtdiary/fragments/add/AddFragment.kt
@@ -0,0 +1,21 @@
+package kaz.dev.thoughtdiary.fragments.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kaz.dev.thoughtdiary.R
+
+
+class AddFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+}

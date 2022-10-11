d18f988524fb81411022df829096dcd244cf1963
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2022-10-11 23:19:40 +0600

    feat: add adapter, show list notes

---
+++ app/src/main/java/kaz/dev/thoughtdiary/fragments/list/ListAdapter.kt
@@ -0,0 +1,61 @@
+package kaz.dev.thoughtdiary.fragments.list
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import kaz.dev.thoughtdiary.R
+import kaz.dev.thoughtdiary.data.models.Priority
+import kaz.dev.thoughtdiary.data.models.ToDoData
+import kaz.dev.thoughtdiary.databinding.RowLayoutBinding
+
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    var dataList = emptyList<ToDoData>()
+
+    class MyViewHolder(val binding: RowLayoutBinding):RecyclerView.ViewHolder(binding.root) {
+        private var title = itemView.findViewById<TextView>(R.id.tvTitleText)
+        private var description = itemView.findViewById<TextView>(R.id.tvDescriptionText)
+        private var priority = itemView.findViewById<Spinner>(R.id.spinnerPriorities)
+
+        fun bind(data: ToDoData) {
+            title.text = data.title
+            description.text = data.description
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = RowLayoutBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+       return  MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+     holder.binding.tvTitleText.text = dataList[position].title
+     holder.binding.tvDescriptionText.text = dataList[position].description
+        when(dataList[position].priority) {
+            Priority.HIGH -> holder.binding.cvPriorityIndicator
+                .setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+            Priority.MEDIUM -> holder.binding.cvPriorityIndicator
+                .setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.blue))
+            Priority.LOW -> holder.binding.cvPriorityIndicator
+                .setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun setData(toDoData: List<ToDoData>) {
+        this.dataList = toDoData
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/kaz/dev/thoughtdiary/fragments/list/ListFragment.kt
+++ app/src/main/java/kaz/dev/thoughtdiary/fragments/list/ListFragment.kt
@@ -3,14 +3,21 @@ package kaz.dev.thoughtdiary.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import kaz.dev.thoughtdiary.R
+import kaz.dev.thoughtdiary.data.viewmodel.ToDoViewModel
 import kaz.dev.thoughtdiary.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     private  var _binding:FragmentListBinding? = null
     private val binding get() = _binding!!
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +40,16 @@ class ListFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, {data->
+            adapter.setData(data)
+        })
+    }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.list_fragment_menu, menu)
     }

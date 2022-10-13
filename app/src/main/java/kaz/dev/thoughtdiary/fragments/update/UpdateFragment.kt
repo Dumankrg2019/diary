e000c140e9298c053bebacad8fccff7f458768bc
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2022-10-14 00:23:36 +0600

    feat: add deleteItem func

--- app/src/main/java/kaz/dev/thoughtdiary/fragments/update/UpdateFragment.kt
+++ app/src/main/java/kaz/dev/thoughtdiary/fragments/update/UpdateFragment.kt
@@ -1,5 +1,6 @@
 package kaz.dev.thoughtdiary.fragments.update
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
@@ -50,8 +51,9 @@ class UpdateFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.menu_save) {
-            updateItem()
+        when(item.itemId) {
+            R.id.menu_save -> updateItem()
+            R.id.menu_delete -> confirmItemRemoval()
         }
         return super.onOptionsItemSelected(item)
     }
@@ -78,5 +80,21 @@ class UpdateFragment : Fragment() {
         } else {
             Toast.makeText(requireActivity(), "Please, fill out all fields!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    // show alert dialog when delete item
+    private fun confirmItemRemoval() {
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setPositiveButton("Yes"){_, _->
+            mToDoViewModel.deleteItem(args.currentItem)
+            Toast.makeText(requireActivity(),
+                "Successfully remove: '${args.currentItem.title}'",
+                Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        builder.setNegativeButton("No"){_,_->}
+        builder.setTitle("Delete ${args.currentItem.title}?")
+        builder.setMessage("Are you sure you want to remove '${args.currentItem.title}'?")
+        builder.create().show()
     }
 }
